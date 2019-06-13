42df9fdb7e9ddf28665b9197d588afe861de7466
Arnon Phayiasawas <arnon.ph@gmail.com>
2019-06-13 19:50:55 +0700

    Update widgets.php

--- inc-functions/widgets.php
+++ inc-functions/widgets.php
@@ -1,18 +1,17 @@
 <?php
 
-function widgets_footer_bar() {
-
+function tcl_widgets_init() {
     register_sidebar( array(
-        array(
-            'name'          => 'Sideber 1',
-            'id'            => 'sidebar',
-            'before_widget' => '<div class="span4">',
-            'after_widget'  => '</div>',
-            'before_title'  => '<h2 class="footer-title">',
-            'after_title'   => '</h2>',
-        )
-    ));
+        'name'          => __( 'Right Sidebar', 'tcl' ),
+        'id'            => 'sidebar-1',
+        'description'   => __( 'Appears in the post and page of the site.', 'tcl' ),
+        'class'         => 'card-body',
+        'before_widget' => '<div class="card mt-3 mb-2 px-3 pb-3">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h5 class="card-header">',
+        'after_title'   => '</h5>',
+    ) );
 
 }
-add_action( 'widgets_init', 'widgets_footer_bar' );
+add_action( 'widgets_init', 'tcl_widgets_init' );
 
